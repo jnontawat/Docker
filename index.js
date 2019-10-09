abfb057bce23f403322e70602f14bc757d472f73
Nontawat Junsane <nontawat.jun@mail.kmutt.ac.th>
2019-10-09 14:03:16 +0700

    add currentDateTime

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const moment = require('moment')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => {
+    const currentDateTime = moment()
+    res.send(`Hello World! <br/>${currentDateTime.format()}`)
+})
 app.listen(port, () => console.log(`Docker Exam on port ${port}!`))
